e0cb3b1eb76afb0d48bc7ec502def22b08cd317c
w0njunipnida <xpbpq9981@gmail.com>
2025-12-02 17:38:55 +0900

    env 생성

--- back/index.js
+++ back/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const pool = require('./db')
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get('/',async(req,res)=>{
+    const user = await pool.query('SELECT * FROM user')
+    res.send(user)
+})
 
 app.listen(8080, ()=>{
     console.log("서버 실행")
