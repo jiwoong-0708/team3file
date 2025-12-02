053f73eef2a93e07f96cc9b1047b92911d42f1f6
jiwoong-0708 <jiwoong0749@gmail.com>
2025-12-02 17:35:49 +0900

    브라우져 생성 및 마이페이지, 로그인 등등 페이지 생성

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from '../team/Home.jsx'
+import Detail from '../team/Detail.jsx'
+import Login from '../team/Login.jsx'
+import Mypage from '../team/Mypage.jsx'
+import Register from '../team/Register.jsx'
+import Wishlist from '../team/Wishlist.jsx'
 import './App.css'
 
 function App() {
@@ -8,6 +14,11 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/detail' element={<Detail />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/mypage' element={<Mypage />}/>
+      <Route path='/register' element={<Regiter />}/>
+      <Route path='/wishlist' element={<Wishlist />}/>
     </Routes>
     </BrowserRouter>
   )
